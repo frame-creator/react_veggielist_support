417f04bc9900a5b7fa1693d5eda36d2c04033272
code-framer <journeyofman@naver.com>
2021-09-30 00:10:07 +0900

    Footer 컴포넌트 추가

---
+++ react_frontend/src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Footer.css';
+
+
+
+  
+const Footer = () => {
+return (
+
+            
+            <div className="foot-section">
+                <p>
+                    Copyright &copy; 2021 by 베지리스트 All rights reserved.
+                </p>
+            </div>
+            
+)
+}
+
+export default Footer;
